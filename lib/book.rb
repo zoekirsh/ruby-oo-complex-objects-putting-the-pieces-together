1ded09c9d9a84295981ce1b84ad2aa41d110648b
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-18 11:50:54 -0800

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+require 'pry'
+
+class Book
+
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        @author
+        @page_count
+        @genre    
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_accessor :condition, :color, :size, :material
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+        @color
+        @size
+        @material
+        @condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+
+
+end
